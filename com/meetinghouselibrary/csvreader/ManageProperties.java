9541f1e92a46b90163b3fa2e5b0f643dea4c623d
Jim Murtha <jim.murtha@msn.com>
2015-02-28 18:48:43 -0700

    Update ManageProperties.java
    example of reading the config.properties file, adding or changing the 'mydatetime' key

--- com/meetinghouselibrary/csvreader/ManageProperties.java
+++ com/meetinghouselibrary/csvreader/ManageProperties.java
@@ -1,5 +1,7 @@
 package com.meetinghouselibrary.csvreader;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.Properties;
 
 /**
@@ -12,9 +14,18 @@ public class ManageProperties {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        SimpleDateFormat sdf = new SimpleDateFormat("MM-dd-yyyy hh:mm:ss");
+        Date curDate = new Date();
         Properties myProps;
         myProps = ManageConfigProperties.getConfigProperties("MyLDSMediaLibrary", "config.properties");
-//        myProps.put("name", "Jimbo");
+
+        if (myProps.containsKey("mydatetime")) {
+            // update property with new date & time
+            myProps.setProperty("mydatetime", sdf.format(curDate));
+        } else {
+            // create new property key
+            myProps.put("mydatetime", sdf.format(curDate));
+        }
         ManageConfigProperties.saveConfigProperties(myProps, "MyLDSMediaLibrary", "config.properties");
     }
     
