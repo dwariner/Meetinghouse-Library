a61f4a8638640b13fcc5998085118b5ac87d85d7
David Wariner <dwariner@Jedi.local>
2015-03-03 07:38:46 -0700

    Updated the CSV exports generated on the server

--- PHP/csv-all.php
+++ PHP/csv-all.php
@@ -1,5 +1,8 @@
 <?php 
 
+	include "csv-relief-society.php";
 	include "csv-mormon-messages.php";
+
+
 
 ?>
--- PHP/csv-relief-society.php
+++ PHP/csv-relief-society.php
@@ -12,7 +12,7 @@
         // output headers so that the file is downloaded rather than displayed
         //header('Content-Type: text/csv; charset=utf-8');
         //header('Content-Disposition: attachment; filename=pikaObs_'.date("Ymd").'.csv');            
-        $filepath = $_SERVER["DOCUMENT_ROOT"] . "/wp-content/uploads/download-manager-files/Mormon Messages.csv";
+        $filepath = $_SERVER["DOCUMENT_ROOT"] . "/wp-content/uploads/download-manager-files/Relief Society.csv";
         //echo $filepath;
 
         // create a file pointer connected to the output stream
@@ -20,7 +20,7 @@
 
 
         // output the column headings
-        fputcsv($output, array('Id','Title','Date','720p','Directory','Category'));
+        fputcsv($output, array('Title','Directory','1080p','720p','360p','File Name','Category','GUID'));
 
         // fetch the data
         mysql_connect($hostname, $username, $password);
@@ -56,27 +56,15 @@ FROM (
         THEN `wp_postmeta`.`meta_value`
         END ) AS `GUID`
 
-
     FROM `wp_posts`
 
-        INNER JOIN `wp_term_relationships` wtr ON (wp_posts.`ID` = wtr.`object_id`)
-        INNER JOIN `wp_term_taxonomy` wtt ON (wtr.`term_taxonomy_id` = wtt.`term_taxonomy_id`)
-        INNER JOIN `wp_terms` wt ON (wt.`term_id` = wtt.`term_id`)
-
+    INNER JOIN `wp_term_relationships` wtr ON (wp_posts.`ID` = wtr.`object_id`)
+    INNER JOIN `wp_term_taxonomy` wtt ON (wtr.`term_taxonomy_id` = wtt.`term_taxonomy_id`)
+    INNER JOIN `wp_terms` wt ON (wt.`term_id` = wtt.`term_id`)
     LEFT JOIN `wp_postmeta` ON ( `wp_posts`.`ID` = `wp_postmeta`.`post_id` )
-
-    WHERE 
-    #`wp_posts`.`post_status` = "publish"
-     `wp_posts`.`post_type` = "post"
-    #AND `wt`.`slug` = "released" 
-    #AND `wtt`.`taxonomy` = "post_tag"
-    AND `wtt`.`taxonomy` = "category" 
-    AND `wt`.`slug`IN ("2014-mormon-messages"
-                                                        ,"2013-mormon-messages"
-                                                        ,"2012-mormon-messages"
-                                                        ,"2011-mormon-messages"
-                                                       ,"2010-mormon-messages"
-                                                       ,"2009-mormon-messages")
+    WHERE `wp_posts`.`post_status` = "publish"
+    AND `wp_posts`.`post_type` = "post"
+    AND wtt.taxonomy = "category" AND wt.`slug`IN ("relief-society")
 
     GROUP BY `wp_posts`.`ID`
 
@@ -87,6 +75,6 @@ FROM (
         // loop over the rows, outputting them
         while ($row = mysql_fetch_assoc($rows)) fputcsv($output, $row);
 
-        exit;
+        //exit;    
 
 ?>
