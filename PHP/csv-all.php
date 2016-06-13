cc411f9d4773d2323f775bd6efa3ddf0579c110c
David Wariner <dwariner@Jedi.local>
2016-06-13 06:36:11 -0600

    Adding 2015 October and 2016 April Libraries info

--- PHP/csv-all.php
+++ PHP/csv-all.php
@@ -5,12 +5,14 @@
 	include "". __DIR__ . "/csv-come-follow-me.php";
 	include "". __DIR__ . "/csv-bible-videos.php";
 	include "". __DIR__ . "/csv-family-history.php";
+	include "". __DIR__ . "/csv-2016-april-general-conference.php";
 	include "". __DIR__ . "/csv-2015-october-general-conference.php";
 	include "". __DIR__ . "/csv-2015-april-general-conference.php";
 	include "". __DIR__ . "/csv-2014-october-general-conference.php";
 	include "". __DIR__ . "/csv-2014-april-general-conference.php";
 	include "". __DIR__ . "/csv-mormon-channel-programs.php";
 	include "". __DIR__ . "/csv-mormon-messages.php";
+	include "". __DIR__ . "/csv-new-testament.php";
 	include "". __DIR__ . "/csv-im-a-mormon.php";
 	include "". __DIR__ . "/csv-prophets-and-apostles.php";
 	include "". __DIR__ . "/csv-hasten-the-work.php";
